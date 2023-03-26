febe965cf7c83b0a2c9f3b8ad6fa739a79356daf
Edward Shanahan <edwardshanahan357@gmail.com>
2023-03-26 19:28:41 +0000

    Practice arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,34 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
 
-// Arrow Function With Parameters
+// function addTwoNumbers(a, b) {
+//   return a + b
+// }
+
+// console.log(addTwoNumbers(1, 2))
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => a + b
+
+console.log(addTwoNumbers(1, 2))
+
 // Implicit Returns
 
+const sayHello = () => console.log("Hello")
+
+sayHello()
+
 // Returning Multiple Lines
+
+const paragraph = () => {
+  return `<p>
+    Hello World!
+    </p>`
+}
+
+console.log(paragraph())
